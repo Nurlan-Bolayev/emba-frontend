cedd884ec4708e700f6dca1abdc288664e60d11e
Nurlan Bolayev <bolayev.nurlan@gmail.com>
2021-01-16 18:38:50 +0400

    Add admin state to vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from '@/axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    admin: null,
   },
   mutations: {
+    setAdmin(state, admin) {
+      state.admin = admin
+    }
   },
   actions: {
+    async fetchUser({commit}) {
+      try {
+        const res = await axios.get('api/admin/user')
+        commit('setAdmin', res.data)
+      } catch (e) {
+        // ignore
+      }
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
