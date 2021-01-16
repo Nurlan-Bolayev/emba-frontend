3d5fe8a96f77df6631adaab6df6a51ad1b1daa94
Nurlan Bolayev <bolayev.nurlan@gmail.com>
2021-01-16 18:55:08 +0400

    Add admin dashboard route

---
+++ src/views/admin/AdminDashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="pa-4 text-center">
+      Welcome, {{ admin && admin.name }}
+    </h1>
+    <div class="pa-6 d-flex justify-center">
+      <v-btn color="primary" class="ma-4" to="/admin/categories">
+        Categories
+      </v-btn>
+      <v-btn color="primary" class="ma-4" to="/admin/products">
+        Products
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    admin: function () {
+      return this.$store.state.admin
+    }
+  },
+}
+</script>
