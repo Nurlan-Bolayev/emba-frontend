a64381f49af3bfb865ec3b20bf34a43366e554df
Nurlan Bolayev <bolayev.nurlan@gmail.com>
2021-01-17 18:34:26 +0400

    Add deleting category

--- src/views/admin/AdminCategories.vue
+++ src/views/admin/AdminCategories.vue
@@ -2,8 +2,18 @@
   <div>
     <h1 class="pa-4 text-center">
       Categories
+      <v-btn
+          to="/admin/categories/new"
+          class="mx-2"
+          fab
+          dark
+          color="primary"
+      >
+        <v-icon dark>
+          mdi-plus
+        </v-icon>
+      </v-btn>
     </h1>
-
     <v-data-table
         :headers="headers"
         :items="categories"
@@ -11,14 +21,24 @@
         class="elevation-4 mx-10 mb-5"
     >
       <template v-slot:item.action="{ item }">
-        <v-btn icon :to="`admin/categories/${item.id}`">
-          <v-icon>
-            $edit
+        <v-btn class="mr-3" small :to="`/admin/categories/${item.id}`" color="success">
+          <v-icon left small>
+            mdi-pencil
           </v-icon>
+          Edit
+        </v-btn>
+
+        <v-btn :loading="categoryBeingDeleted === item.id" @click="deleteCategory(item.id)"
+               color="error"
+               depressed
+               small
+        >
+          Delete
         </v-btn>
       </template>
+
       <template v-slot:item.updated_at="{value}">
-        {{new Date(value).toDateString()}}
+        {{ new Date(value).toDateString() }}
       </template>
     </v-data-table>
   </div>
@@ -39,14 +59,26 @@ export default {
       ],
       categories: [],
       isLoading: false,
+      categoryBeingDeleted: null,
     }
   },
+  methods: {
+    async deleteCategory(id) {
+      try {
+        this.categoryBeingDeleted = id
+        await axios.delete(`api/admin/categories/${id}`);
+        this.categories = this.categories.filter(category => category.id !== id);
+      } finally {
+        this.categoryBeingDeleted = null;
+      }
+    }
+  },
+
   async mounted() {
     this.isLoading = true
     const res = await axios.get('api/admin/categories');
     this.categories = res.data;
     this.isLoading = false;
   }
-
 }
 </script>
